083d31c717dd74d0a039c037a7a1211daac249fe
Anastasia Hanuliak <anastasia.hanuliak@gmail.com>
2019-05-03 13:02:40 +0300

    modified app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { NavComponent } from './nav/nav.component';
+import { FilterComponent } from './filter/filter.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { SpGeneralComponent } from './sp-general/sp-general.component';
+import { QuoteComponent } from './quote/quote.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavComponent,
+    FilterComponent,
+    FooterComponent,
+    PaginationComponent,
+    SpGeneralComponent,
+    QuoteComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
