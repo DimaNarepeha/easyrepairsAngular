4a440bd826f75ef640b701ea14bb045a6a5ee2ca
DimaNarepeha <dimanarepeha@gmail.com>
2019-05-10 13:05:44 +0300

    JAVALV-174,175 add simple design for notifications

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-notification',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  openNotifications() {
+    console.log('CLICKED');
+  }
 }
