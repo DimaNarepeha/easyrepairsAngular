117dda6e551305a88712712a801465505103bb2a
DimaNarepeha <dimanarepeha@gmail.com>
2019-05-20 12:36:11 +0300

    Add Constant BaseURL

--- src/app/notification/notification.service.ts
+++ src/app/notification/notification.service.ts
@@ -1,18 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  private url = 'http://localhost:8080/notification/get/'; // TODO change hardcoded localhost
+  private readonly baseURL; // TODO change hardcoded localhost
 
 
   constructor(private http: HttpClient) {// TODO change to https here
+    this.baseURL = environment.baseURL + '/notification/get/';
   }
 
   getNotificationsForUser(id: number): Observable<Notification[]> {
-    return this.http.get<Notification[]>(this.url + id);
+    return this.http.get<Notification[]>(this.baseURL + id);
   }
 }
