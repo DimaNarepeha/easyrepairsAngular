2f756cf05ffb12f8df8e639828b02ea740c71dd1
Anast <nastya_strong@ukr.net>
2019-05-20 18:57:59 +0300

    JAVALV100::add refactoring

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Customer} from './customer';
 import {CustomerService} from './customer.service';
 import {DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'customers',
@@ -20,7 +20,7 @@ export class CustomerComponent implements OnInit {
   public userFile: any = File;
   public im: any;
 
-  constructor(private _customerService: CustomerService, private _sanitizer: DomSanitizer) {
+  constructor(private customerService: CustomerService, private _sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
@@ -36,17 +36,17 @@ export class CustomerComponent implements OnInit {
 
 
   formGroup: FormGroup = new FormGroup({
-    firstname: new FormControl(null,[
+    firstname: new FormControl(null, [
       Validators.required,
       Validators.minLength(2),
       Validators.maxLength(35)
     ]),
-    lastname: new FormControl(null,[
+    lastname: new FormControl(null, [
       Validators.required,
       Validators.minLength(10),
       Validators.maxLength(255)
     ]),
-    email: new FormControl(null,[
+    email: new FormControl(null, [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(60),
@@ -56,7 +56,7 @@ export class CustomerComponent implements OnInit {
 
 
   serveImage(image: string) {
-    return this._customerService.getImage(image).subscribe(res => {
+    return this.customerService.getImage(image).subscribe(res => {
       this.im = this._sanitizer.bypassSecurityTrustUrl(res);
       console.log(this.im);
 
@@ -67,32 +67,26 @@ export class CustomerComponent implements OnInit {
 
   onSelectFile(event, id) {
     const file = event.target.files[0];
-    /*const fd = new FormData();
-     fd.append('image',file);
-     console.log(file);*/
     console.log(file);
     this.userFile = file;
-    this._customerService.uploadImage(file, id).subscribe(res => {
+    this.customerService.uploadImage(file, id).subscribe(res => {
       console.log(res);
       this.getCustomersByPage();
     });
-    ;
-
   }
 
   getCustomersByPage() {
-    return this._customerService.getCustomersPage(this.page).subscribe(
+    return this.customerService.getCustomersPage(this.page).subscribe(
       data => {
         console.log(data);
-        let d = data.json();
+        const d = data.json();
         console.log(d);
-        //console.log("result = " + d.result);
         this.customerPage = d['content'];
         this.pages = new Array(d['totalPages']);
         console.log(data['content']);
         console.log(this.pages);
-        console.log(this.customerPage)
-        console.log(data)
+        console.log(this.customerPage);
+        console.log(data);
       },
       (error) => {
         console.log(error);
@@ -101,9 +95,9 @@ export class CustomerComponent implements OnInit {
   }
 
   getCustomers(): void {
-    this._customerService.getAllCustomers().subscribe(
+    this.customerService.getAllCustomers().subscribe(
       (customerData) => {
-        this.customers = customerData, console.log(customerData)
+        this.customers = customerData; console.log(customerData);
       },
       (error) => {
         console.log(error);
@@ -112,13 +106,15 @@ export class CustomerComponent implements OnInit {
   }
 
   addCustomer(): void {
-    this._customerService.addCustomer(this.customer)
+    this.customerService.addCustomer(this.customer)
       .subscribe((response) => {
           console.log(response);
           this.reset();
           this.getCustomersByPage();
+          alert('customer added/updated!');
         },
         (error) => {
+          alert('Invalid data provided!');
           console.log(error);
         });
   }
@@ -127,15 +123,18 @@ export class CustomerComponent implements OnInit {
     this.customer.firstName = null;
     this.customer.lastName = null;
     this.customer.email = null;
+    this.customer.image = '1.jpg';
   }
 
   deleteCustomer(customerId: string) {
-    this._customerService.deleteCustomer(customerId)
+    this.customerService.deleteCustomer(customerId)
       .subscribe((response) => {
           console.log(response);
+          alert('customer deleted!');
           this.getCustomersByPage();
         },
         (error) => {
+          alert('Invalid data provided!');
           console.log(error);
         });
 
@@ -143,11 +142,12 @@ export class CustomerComponent implements OnInit {
 
 
   getCustomerById(customerId: string) {
-    this._customerService.getCustomerById(customerId)
+    this.customerService.getCustomerById(customerId)
       .subscribe((customerData) => {
         this.customer = customerData;
         this.getCustomers();
       }, (error) => {
+        alert('Invalid data provided!');
         console.log(error);
       });
   }
--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -11,56 +11,47 @@ import { environment } from 'src/environments/environment';
 
 
 @Injectable()
-export class CustomerService{
-    
-    constructor(private _httpService: Http,private _sanitizer: DomSanitizer){};
+export class CustomerService {
+    constructor(private httpService: Http) { }
 
-        uploadImage(file:any,id:number){
+        uploadImage(file: any, id: number) {
         const formData = new FormData();
-        formData.append('imageFile',file);
-        return  this._httpService.post(environment.customer_url+id+"/image",formData);
-       
+        formData.append('imageFile', file);
+        return  this.httpService.post(environment.customer_url + id + '/image', formData);
     }
 
-    getImage(image:string):Observable<any>{
-       /* const params = new HttpParams()
-        .append('imageName', image);*/
-        
-           
-                return this._httpService.get(environment.customer_url+"image/"+image);
+    getImage(image: string): Observable<any> {
+                return this.httpService.get(environment.customer_url + 'image/' + image);
     }
 
-    getCustomersPage(page:Number){
-        
-        return  this._httpService.get(environment.customer_url+"list?page="+page);
+    getCustomersPage(page: number) {
+        return  this.httpService.get(environment.customer_url + 'list?page=' + page);
     }
-    getAllCustomers(): Observable <Customer[]>{
-           return this._httpService.get(environment.customer_url).
+    getAllCustomers(): Observable <Customer[]> {
+           return this.httpService.get(environment.customer_url).
             map( (response: Response) => response.json()).catch(this.handleError);
     }
-   
-    addCustomer(customer:Customer){
-        let body = JSON.stringify(customer);
-        let headers = new Headers({'Content-Type':'application/json'});
-        let options = new RequestOptions({headers: headers});
-        if(customer.id){
-            let c = customer.id;
-            customer.id = null; 
-            return this._httpService.put(environment.customer_url+c,body,options);
-            
+    addCustomer(customer: Customer) {
+        const body = JSON.stringify(customer);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        const options = new RequestOptions({headers: headers});
+        if (customer.id) {
+            const c = customer.id;
+            customer.id = null;
+            return this.httpService.put(environment.customer_url + c, body, options);
+        } else {return this.httpService.post(environment.customer_url, body, options);
         }
-        else{return this._httpService.post(environment.customer_url,body,options);}
     }
-    deleteCustomer(customerId:string){
-       return this._httpService.delete(environment.customer_url+customerId);
+    deleteCustomer(customerId: string) {
+       return this.httpService.delete(environment.customer_url + customerId);
 }
 
-    getCustomerById(customerId:string):Observable<Customer>{
-           return  this._httpService.get(environment.customer_url+customerId)
-            .map((response: Response)=>response.json())
+    getCustomerById(customerId: string): Observable<Customer> {
+           return  this.httpService.get(environment.customer_url + customerId)
+            .map((response: Response) => response.json())
             .catch(this.handleError);
     }
-    private handleError(error: Response){
+    private handleError(error: Response) {
             return Observable.throw(error);
     }
 }
