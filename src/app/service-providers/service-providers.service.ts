4d91a277b5dbf72c13ad96811ecd45e163072ff1
serhiylev <slevitsku@gmail.com>
2019-05-23 10:34:25 +0300

    changed links

--- src/app/service-providers/service-providers.service.ts
+++ src/app/service-providers/service-providers.service.ts
@@ -92,7 +92,7 @@ export class ServiceProvidersService {
     let headers = new Headers({'Content-Type': 'application/json'});
     let options = new RequestOptions({headers: headers});
     console.log('id ' + id + '  status  ' + providerDTO.status);
-    return this.httpService.put('http://localhost:8080/service-providers/update-status/' + id, body)
+    return this.httpService.put(this.baseURL +'/service-providers/update-status/' + id, body)
       .map((response: Response) => response.json());
   }
 
@@ -100,8 +100,7 @@ export class ServiceProvidersService {
     let statusString: string = ProviderStatus[status];
     let params = new HttpParams().set('numberOfProvidersOnPage', String(numberOfProvidersOnPage))
       .set('page', String(page)).set('status', statusString);
-    console.log(this.httpService.get(`http://localhost:8080/service-providers/find-all/status?` + params));
-    return this.http.get<ServiceProviders[]>(`http://localhost:8080/service-providers/find-all/status?` + params)
+    return this.http.get<ServiceProviders[]>( this.baseURL + `/service-providers/find-all/status?` + params)
       .pipe(
         map(res => res['content']));
   }
