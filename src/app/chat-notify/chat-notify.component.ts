8333d3a702e33256d364f93c9749155036399c29
Anast <nastya_strong@ukr.net>
2019-06-20 17:09:27 +0300

    add chat notifier

--- src/app/chat-notify/chat-notify.component.ts
+++ src/app/chat-notify/chat-notify.component.ts
@@ -3,6 +3,7 @@ import {Http, Response} from "@angular/http";
 import {environment} from "../../environments/environment";
 import {Chat} from "../chat/chat";
 import {SecurityRolesService} from "../security-roles.service";
+import {NotifierService} from "angular-notifier";
 
 @Component({
   selector: 'app-chat-notify',
@@ -10,36 +11,40 @@ import {SecurityRolesService} from "../security-roles.service";
   styleUrls: ['./chat-notify.component.css']
 })
 export class ChatNotifyComponent implements OnInit {
+  private readonly notifier: NotifierService;
   isOpened = false;
-  id:any;
+  empty:boolean;
+  id: any;
   unreadChatsForUser: Chat[];
-  url = environment.baseURL +"/service-providers/image/";
-  constructor(private httpService: Http,private src: SecurityRolesService) { }
+  url = environment.baseURL + "/service-providers/image/";
+  constructor(private httpService: Http,private src: SecurityRolesService, notifierService: NotifierService) {
+    this.notifier = notifierService;
+  }
   getUnreadMessagesForAUser(sendTo:any){
-    this.httpService.get(environment.baseURL+"/message/getUnreadForUser/" +sendTo ).map((response: Response) => response.json())
+    this.httpService.get(environment.baseURL + "/message/getUnreadForUser/" + sendTo ).map((response: Response) => response.json())
       .subscribe((response) => {
         console.log(response);
         this.unreadChatsForUser = response;
       });
-
+    if (this.unreadChatsForUser && this.unreadChatsForUser.length===0){
+      this.notifier.notify('success', 'No messages for you!');
+    }
   }
   ngOnInit() {
-   this.id = this.src.getUserId();
-
   }
   @HostListener('click')
   clickOutside() {// if clicked outside the div then close the notification block
     this.isOpened = !this.isOpened;
   }
 
-  reverse(){
+  reverse() {
     this.isOpened = false;
   }
-  /*openNotifications() {
-    this.isOpened = true;
-  }*/
   openNotifications(divNotifications: HTMLDivElement) {
+    this.id = this.src.getUserId();
     this.isOpened = true;
     this.getUnreadMessagesForAUser(this.id);
+
+
   }
 }
